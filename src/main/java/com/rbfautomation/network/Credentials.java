c40cbf18120e54de6781b587916754f69d8416a8
Brian Floersch <gh123man@gmail.com>
2015-03-04 14:33:06 -0500

    fixed repo structure

---
+++ src/main/java/com/rbfautomation/network/Credentials.java
@@ -0,0 +1,10 @@
+package com.rbfautomation.network;
+
+/**
+ * Created by brian on 1/31/15.
+ */
+public class Credentials {
+    protected static String KEYSTORE_PASSWORD = "eb721c07ae1a118e3a4a9455d7b1ae53";
+    protected static String HOST = "https://automation.sb1.io";
+    protected static int    PORT   = 443;
+}
